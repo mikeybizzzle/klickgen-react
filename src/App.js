e234ee1d9637ba26b01114eb483cbb8e54c8e95f
mikeybizzzle <MIKEYATHLETICS@GMAIL.COM>
2025-08-07 23:44:51 -0400

    Remove tsParticles; add CSS animated hero bg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Particles from 'react-tsparticles';
-import { loadFull } from 'tsparticles';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import Process from './components/Process';
@@ -8,30 +6,11 @@ import Work from './components/Work';
 import CTA from './components/CTA';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import './style.css';
 
-function App() {
-  const particlesInit = async (main) => {
-    await loadFull(main);
-  };
-
+export default function App() {
   return (
     <>
-      <Particles init={particlesInit} options={{
-        fpsLimit: 60,
-        particles: {
-          number: { value: 45 },
-          color: { value: '#00ff94' },
-          shape: { type: 'circle' },
-          opacity: { value: 0.35 },
-          size: { value: { min: 1, max: 4 } },
-          move: { enable: true, speed: 0.6, outModes: 'out' },
-        },
-        interactivity: {
-          events: { onHover: { enable: true, mode: 'repulse' } },
-          modes: { repulse: { distance: 75 } },
-        },
-        fullScreen: { enable: false },
-      }} />
       <Hero />
       <Services />
       <Process />
@@ -42,5 +21,3 @@ function App() {
     </>
   );
 }
-
-export default App;
